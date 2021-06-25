e350da94b5c8cda49504e0357d4ee4a3326cf49a
Henrique Pereira <henrique.pereira@sapium.com.br>
2021-06-25 12:12:35 -0300

    Finish test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,27 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if (!!newTaskTitle) {
+      setTasks([...tasks, {
+        id: new Date().getTime(),
+        done: false,
+        title: newTaskTitle
+      }])
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    setTasks(tasks.map(task => ({
+      ...task,
+      done: task.id === id ? !task.done : task.done
+    })))
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks.filter(task => task.id !== id))
   }
 
   return (
